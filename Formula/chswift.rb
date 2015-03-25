66ef176ca98229a0c897bd9dc7abfac613d2ba78
Boris Bügling <boris@icculus.org>
2015-03-25 19:42:40 +0100

    Added formula for chswift

---
+++ Formula/chswift.rb
@@ -0,0 +1,23 @@
+require 'formula'
+
+class Chswift < Formula
+  homepage 'https://github.com/neonichu/chswift#readme'
+  url "https://github.com/neonichu/chswift.git", :tag => "0.0.1"
+  head 'https://github.com/neonichu/chswift.git'
+
+  def install
+    system 'make', 'install', "PREFIX=#{prefix}"
+  end
+
+  def caveats; <<-EOS.undent
+    Add the following to the ~/.bashrc or ~/.zshrc file:
+
+      source #{opt_share}/chswift/chswift.sh
+
+    To enable auto-switching of Swifts specified by .swift-version files,
+    add the following to ~/.bashrc or ~/.zshrc:
+
+      source #{opt_share}/chswift/auto.sh
+    EOS
+  end
+end
