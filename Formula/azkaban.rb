b1b63d4b324ed5488f2a28f948397f03785db4a4
Boris Bügling <boris@icculus.org>
2015-12-21 21:19:30 +0100

    Fix azkaban formula

--- Formula/azkaban.rb
+++ Formula/azkaban.rb
@@ -3,11 +3,12 @@ class Azkaban < Formula
   url "https://github.com/neonichu/azkaban.git", :tag => "0.1.0"
   head "https://github.com/neonichu/azkaban.git"
 
+  depends_on 'chswift'
   depends_on :xcode => ["7.2"]
 
   def install
     system 'sed', '-i', '', 's/#import "Alcatraz.h"//', 'Externals/Alcatraz/Alcatraz/Packages/ATZPackage.m'
-    system 'make', '.build/debug/azkaban'
+    system 'chswift-exec', '2.2', '--', 'make', '.build/debug/azkaban'
     system 'cp', '.build/debug/azkaban', "#{prefix}/bin"
   end
 
