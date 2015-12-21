6f087b423f58c33be5fedf0b0666e03ac9ec0ccb
Boris Bügling <boris@icculus.org>
2015-12-21 21:04:03 +0100

    Add formula for azkaban

---
+++ Formula/azkaban.rb
@@ -0,0 +1,16 @@
+class Xctester < Formula
+  homepage "https://github.com/neonichu/azkaban"
+  url "https://github.com/neonichu/azkaban.git", :tag => "0.1.0"
+  head "https://github.com/neonichu/azkaban.git"
+
+  depends_on :xcode => ["7.2"]
+
+  def install
+    system 'make', '.build/debug/azkaban'
+    system 'cp', '.build/debug/azkaban', "#{prefix}/bin"
+  end
+
+  test do
+    system "#{bin}/azkaban", "--version"
+  end
+end
