7b50bae935a47decf3b2dca7cf9bdb1842d62981
Boris Bügling <boris@icculus.org>
2015-12-29 10:59:45 +0100

    Bump version of azkaban

--- Formula/azkaban.rb
+++ Formula/azkaban.rb
@@ -1,13 +1,12 @@
 class Azkaban < Formula
   homepage "https://github.com/neonichu/azkaban"
-  url "https://github.com/neonichu/azkaban.git", :tag => "0.1.0"
+  url "https://github.com/neonichu/azkaban.git", :tag => "0.2.0"
   head "https://github.com/neonichu/azkaban.git"
 
   depends_on 'chswift'
   depends_on :xcode => ["7.2"]
 
   def install
-    system 'sed', '-i', '', 's/#import "Alcatraz.h"//', 'Externals/Alcatraz/Alcatraz/Packages/ATZPackage.m'
     system 'chswift-exec', '2.2', '--', 'make', '.build/debug/azkaban'
     system 'mkdir', '-p', "#{prefix}/bin"
     system 'cp', '.build/debug/azkaban', "#{prefix}/bin/azkaban"
