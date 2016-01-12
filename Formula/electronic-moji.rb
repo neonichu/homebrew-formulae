7636214912a94a85060f3b9c6d1028af1d271e21
Boris Bügling <boris@icculus.org>
2016-01-12 22:12:55 +0100

    Add formula for electronic-moji

---
+++ Formula/electronic-moji.rb
@@ -0,0 +1,18 @@
+class ElectronicMoji < Formula
+  homepage "https://github.com/neonichu/electronic-moji"
+  url "https://github.com/neonichu/electronic-moji.git", :tag => "0.0.2"
+  head "https://github.com/neonichu/electronic-moji.git"
+
+  depends_on 'chswift'
+  depends_on :xcode => ["7.2"]
+
+  def install
+    system 'chswift-exec', '2.2', '--', 'make', '.build/debug/electronic-moji'
+    system 'mkdir', '-p', "#{prefix}/bin"
+    system 'cp', '.build/debug/electronic-moji', "#{prefix}/bin/electronic-moji"
+  end
+
+  test do
+    system "#{bin}/electronic-moji", "--version"
+  end
+end
