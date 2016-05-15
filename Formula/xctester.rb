e7a4e3a1b74819fab6d5a631413016140be6633a
Boris Bügling <boris@icculus.org>
2016-05-15 18:17:26 +0200

    Bump version of xctester

--- Formula/xctester.rb
+++ Formula/xctester.rb
@@ -1,9 +1,9 @@
 class Xctester < Formula
   homepage "https://github.com/neonichu/xctester"
-  url "https://github.com/neonichu/xctester.git", :tag => "0.2.0"
+  url "https://github.com/neonichu/xctester.git", :tag => "0.3.0"
   head "https://github.com/neonichu/xctester.git"
 
-  depends_on :xcode => ["7.0.1"]
+  depends_on :xcode => ["7.3.1"]
 
   def install
     prefix.install "bin"
