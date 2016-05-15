d0b40c8205e03ddae26c7418574c47a1cb50a856
Boris Bügling <boris@icculus.org>
2016-05-15 18:22:10 +0200

    Bump version of cato

--- Formula/cato.rb
+++ Formula/cato.rb
@@ -1,6 +1,6 @@
 class Cato < Formula
   homepage "https://github.com/neonichu/cato"
-  url "https://github.com/neonichu/cato.git", :tag => "0.1.1"
+  url "https://github.com/neonichu/cato.git", :tag => "0.2.0"
   head "https://github.com/neonichu/cato.git"
 
   depends_on 'chswift'
